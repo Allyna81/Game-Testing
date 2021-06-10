1ebf4813c03bb6adb37ec6ebd68abac6787b6f5f
ChrisBenetto <chrisbenetto@gmail.com>
2021-06-10 16:11:15 +0200

    End of createDb

--- backend/API/migrations/deploy/createdb.sql
+++ backend/API/migrations/deploy/createdb.sql
@@ -40,8 +40,31 @@ CREATE TABLE "message" IF NOT EXISTS (
     "create_date" TIMESTAMPTZ DEFAULT NOW(),
     "userId" INT REFERENCES "user"("id")
 );
-CREATE TABLE "review" IF NOT EXISTS
-CREATE TABLE "game_has_plateform" IF NOT EXISTS
-CREATE TABLE "game_has_category" IF NOT EXISTS
+CREATE TABLE "review" IF NOT EXISTS ( 
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "content" TEXT,			
+    "gameplay_note"	INT	NOT NULL,	
+    "soundtrack_note" INT NOT NULL,	
+    "graphism_note"	INT	NOT NULL,	
+    "global_note" INT NOT NULL,	
+    "up_vote" INT DEFAULT 0,
+    "down_vote"	 INT DEFAULT 0,	
+    "report" INT DEFAULT 0,	
+    "create_date" TIMESTAMPTZ DEFAULT NOW(),	
+    "update_date" TIMESTAMPTZ,	
+    "userId" INT NOT NULL REFERENCES "user"("id"),	
+    "gameId" INT NOT NULL REFERENCES "game"("id"),			
+    "plateform"	TEXT NOT NULL
+);
+CREATE TABLE "game_has_plateform" IF NOT EXISTS (
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "plateformId" INT NOT NULL REFERENCES "plateform"("id"),
+    "gameId" INT NOT NULL REFERENCES "game"("id")
+);
+CREATE TABLE "game_has_category" IF NOT EXISTS (
+    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "categoryId" INT NOT NULL REFERENCES "category"("id"),
+    "gameId" INT NOT NULL REFERENCES "game"("id")
+);
 
 COMMIT;
