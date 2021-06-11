fed46b47add5929f11481bf5053f74c123a12dc5
Allyna81 <remy.fournier@outlook.com>
2021-06-11 15:59:03 +0200

    React starting base

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // == Import : npm
 import React from 'react';
 import { render } from 'react-dom';
+// import { Provider } from 'react-redux';
+// import store from 'src/store';
 
 // == Import : local
 // Composants
