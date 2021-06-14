31af03bf8de6a32f74c8eae4b6707ba0ee4a4122
Allyna81 <remy.fournier@outlook.com>
2021-06-14 15:27:29 +0200

    Main page React with no dynamic data

---
+++ src/components/MainPage/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { Card, Header, Button } from 'semantic-ui-react';
+import GameCard from './GameCard';
+import './style.scss';
+
+const mainPage = () => (
+  <div className="main">
+    <div className="main-card-category">
+      <Header as='h2'>Most Popular</Header>
+      <Button>View All</Button>
+    </div>
+    <div>
+      <Card.Group itemsPerRow={2}>
+        <GameCard />
+        <GameCard />
+      </Card.Group>
+    </div>
+    <div className="main-card-category">
+      <Header as='h2'>Top Rated</Header>
+      <Button>View All</Button>
+    </div>
+    <div>
+      <Card.Group itemsPerRow={2}>
+        <GameCard />
+        <GameCard />
+      </Card.Group>
+    </div>
+    <div className="main-card-category">
+      <Header as='h2'>New Releases</Header>
+      <Button>View All</Button>
+    </div>
+    <div>
+      <Card.Group itemsPerRow={2}>
+        <GameCard />
+        <GameCard />
+      </Card.Group>
+    </div>
+  </div>
+);
+
+export default mainPage;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import 'semantic-ui-css/semantic.min.css';
 
-import App from './components/App';
+import MainPage from './components/MainPage';
 import reducers from './reducers';
 
 const store = createStore(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <MainPage />
   </Provider>,
   document.getElementById('root'),
 );
