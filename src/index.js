632cd808afe397136b16280c162cf376873c2b35
SalouaK <saloua.khettab@gmail.com>
2021-06-14 09:30:12 +0200

    Initialisation with redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-// == Import : npm
 import React from 'react';
-import { render } from 'react-dom';
-// import { Provider } from 'react-redux';
-// import store from 'src/store';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-// == Import : local
-// Composants
-import App from 'src/components/App';
+import App from './components/App';
+import reducers from './reducers';
 
-// == Render
-// 1. Élément React racine (celui qui contient l'ensemble de l'app)
-//    => crée une structure d'objets imbriqués (DOM virtuel)
-const rootReactElement = <App />;
-// 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
-const target = document.getElementById('root');
-// 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
-render(rootReactElement, target);
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
