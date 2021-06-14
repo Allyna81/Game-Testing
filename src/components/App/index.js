a10354c581bddb9702c5d0acac665f0ad2a67e43
unknown <bryan.catiche@gmail.com>
2021-06-14 16:43:44 +0200

    footer with Route test , without dataRoute

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,23 @@
 // == Import npm
 import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import Footer from '../footer';
+import privacy from '../footer/privacy';
+import website from '../footer/website';
+import about from '../footer/about';
+import { BrowserRouter as Router , Switch , Route } from 'react-router-dom';
 
 // == Composant
 const App = () => (
-  <div>
-    <h1>Terrain prêt </h1>
-  </div>
-);
+    <Router>
+        <Footer /> 
+    <Switch>
+      <Route path="/privacy" component={privacy} />
+      <Route path="/website" component={website} />
+      <Route path="/about" component={about} />
+    </Switch>
+    </Router>
+  );
 
 // == Export
 export default App;
---
+++ src/components/footer/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import "./Footer.css";
+
+function Footer() {
+    return (
+      <footer>
+      <div className="main-footer">
+        <div className="container">
+        <div className="foot-logo"/>
+        <nav className="foot-link">
+            <ul>
+                <Link to='/privacy'><li className="foot-item">Privacy Policy</li></Link>
+                <Link to='/website'><li className="foot-item">Website</li></Link>
+                <Link to='/about'><li className="foot-item">About Us</li></Link>
+            </ul>
+        </nav>
+          <div className="row">
+            <p className="foot-copyright">
+            Game Testing Interactive Entertainment GT., &copy;{new Date().getFullYear()} </p>
+          </div>
+        </div>
+      </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
