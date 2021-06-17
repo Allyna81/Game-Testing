9eeaffd3569dca27d2c6911f2fab3bb171f39f53
unknown <bryan.catiche@gmail.com>
2021-06-17 22:03:42 +0200

    modif

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import "./Footer.css";
+import { Link } from 'react-router-dom';
+import './Footer.css';
 
 function Footer() {
-    return (
-      <footer>
+  return (
+    <footer>
       <div className="main-footer">
         <div className="container">
-        <div className="foot-logo"/>
-        <nav className="foot-link">
+          <div className="foot-logo" />
+          <nav className="foot-link">
             <ul>
-                <Link to='/privacy'><li className="foot-item">Privacy Policy</li></Link>
-                <Link to='/website'><li className="foot-item">Website</li></Link>
-                <Link to='/about'><li className="foot-item">About Us</li></Link>
+              <Link to="/privacy"><li className="foot-item">Privacy Policy</li></Link>
+              <Link to="/website"><li className="foot-item">Website</li></Link>
+              <Link to="/about"><li className="foot-item">About Us</li></Link>
             </ul>
-        </nav>
+          </nav>
           <div className="row">
             <p className="foot-copyright">Game Testing Interactive Entertainment GT., &copy;{new Date().getFullYear()} </p>
           </div>
         </div>
       </div>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+    </footer>
+  );
+}
+
+export default Footer;
