6ac4af9a153f4b4afbd78f1450ffbbec6661b808
Allyna81 <remy.fournier@outlook.com>
2021-06-22 11:47:33 +0200

    review & css & button done

--- src/components/Reviews/Review.js
+++ src/components/Reviews/Review.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FormField } from 'semantic-ui-react';
-import { Card, Form, Rating, Feed } from 'semantic-ui-react';
+import { Container, Form, Rating, Feed, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import './style.scss';
 
 export default class Review extends Component {
@@ -21,41 +22,47 @@ export default class Review extends Component {
   render() {
     return (
     <div className="review-container">
-      <Card className="write-review-container">
-        <Form>
-          <Form.TextArea label='Your review' placeholder='Write your review here...' />
+    <Link to={'/games/:id'}>
+        <div className="back-page-icon-bis">
+          <Icon link name='angle left' />
+          <p>Back to game</p>
+        </div>
+      </Link>
+      <Container className="write-review-container">
+        <Form className="review-form">
+          <Form.TextArea className="review-label" label='Your review' placeholder='Write your review here...' />
           <FormField>
-          <div>
+          <div className="write-review-rating-container global-rate">
             <Rating maxRating={5} onRate={this.handleChangeGlobalRate} />
             <p>Global rate</p>
             <pre>{JSON.stringify(this.state, null, 2)}</pre>
           </div>
-          <div>
+          <div className="write-review-rating-container">
             <Rating maxRating={5} onRate={this.handleChangeGameplay} />
             <p>Gameplay</p>
             <pre>{JSON.stringify(this.state, null, 2)}</pre>
           </div>
-          <div>
+          <div className="write-review-rating-container">
             <Rating maxRating={5} onRate={this.handleChangeSoundtrack} />
             <p>Soundtrack</p>
             <pre>{JSON.stringify(this.state, null, 2)}</pre>
           </div>
-          <div>
+          <div className="write-review-rating-container">
             <Rating maxRating={5} onRate={this.handleChangeGraphics} />
             <p>Graphics</p>
             <pre>{JSON.stringify(this.state, null, 2)}</pre>
           </div>
           </FormField>
-          <Form.Button>Send</Form.Button>
+          <Form.Button className="review-button">Send</Form.Button>
         </Form>
-      </Card>
-      <div className="reviews">
+      </Container>
+      <Container className="reviews">
           <Feed>
             <Feed.Event>
               <Feed.Label image='https://react.semantic-ui.com/images/avatar/small/laura.jpg' />
             <Feed.Content>
             <Feed.Summary>
-              <a>Laura Faucet</a>
+              <a className="summary-profile">Laura Faucet</a>
             </Feed.Summary>
             <Feed.Date>posted on July 9th 2021</Feed.Date>
               <Feed.Extra text>
@@ -77,12 +84,16 @@ export default class Review extends Component {
               <Rating defaultRating={5} maxRating={5} disabled />
               <p>Graphics</p> 
             </div>
-            <div className="rating-container">
+            <div className="rating-container global-rate">
               <Rating defaultRating={5} maxRating={5} disabled />
               <p>Global rate</p> 
             </div>
+            <div className="report-icon">
+              <Icon link name='exclamation' />
+              <p>report</p>
+            </div>
           </div>
-      </div>
+      </Container>
     </div>
     )
   }
