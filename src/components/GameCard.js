0f0281a4f11c3fc14151c52454eb1e61313707f4
Allyna81 <remy.fournier@outlook.com>
2021-06-23 18:56:14 +0200

    axios on main page done

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -4,36 +4,53 @@ import { Card, Rating } from 'semantic-ui-react';
 import './style.scss';
 import { Link } from 'react-router-dom';
 
-const GameCard = () => (
+const GameCard = ({ game }) => {
+  //console.log('rep result', results);
+  return (
   <Card className="main-card">
     <div className="game-img-container">
+  
     <Link to={'/games/:id'}> 
       <img 
-        src="https://image.jeuxvideo.com/medias/158826/1588264398-815-jaquette-avant.jpg"
+        src={game.cover.url}
         alt="logo"
         />
     </Link>
     </div>
     
     <Card.Content className="info-container">
-      <Card.Header className="card-title">Assassin's Creed Valhalla</Card.Header>
+      <Card.Header className="card-title">{game.name}</Card.Header>
       <Card.Meta>
-        <span className="develop-by">Ubisoft</span>
+        {game.involved_companies.map((develop) => {
+          return (
+            <span className="develop-by">{develop.company.name}</span>
+          )
+        })}
       </Card.Meta>
 
       <Card.Content className="tags-container">
-        <div className="ui mini label platform">
-          PS5
-        </div>
-        <div className="ui mini label gametype">
-          action
-        </div>
+        
+          {game.platforms.map((platform) => {
+            return (
+              <div className="ui mini label platform" >
+                {platform.name}
+              </div>
+            )
+          })}
+          {game.genres.map((type) => {
+            return (
+              <div className="ui mini label gametype">
+                {type.name}
+              </div>
+            )
+          })}
         <div className="rating-container">
           <Rating defaultRating={3} maxRating={5} disabled />
         </div>
       </Card.Content>
     </Card.Content>
   </Card>
-);
+  );
+};
 
 export default GameCard;
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,42 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 // import PropTypes from 'prop-types';
 import { Card, Header, Button } from 'semantic-ui-react';
 import GameCard from './GameCard';
 import './style.scss';
 
-const MainPage = ({ viewAllGamesButton }) => (
-  <div className="main mt-5">
-    <div className="main-card-category">
-      <Header as='h2'>Most Popular</Header>
-      <Button  className="view-all-button" onClick={viewAllGamesButton}>View All<span className="button-span">+</span></Button>
-    </div>
-    <div>
-      <Card.Group className="" itemsPerRow={2}>
-        <GameCard />
-        <GameCard />
-      </Card.Group>
-    </div>
-    <div className="main-card-category">
-      <Header as='h2'>Top Rated</Header>
-      <Button className="view-all-button" onClick={viewAllGamesButton}>View All <span className="button-span">+</span></Button>
-    </div>
-    <div>
-      <Card.Group itemsPerRow={2}>
-        <GameCard />
-        <GameCard />
-      </Card.Group>
-    </div>
-    <div className="main-card-category">
-      <Header as='h2'>New Releases</Header>
-      <Button className="view-all-button" onClick={viewAllGamesButton}>View All <span className="button-span">+</span></Button>
-    </div>
-    <div>
-      <Card.Group itemsPerRow={2}>
-        <GameCard />
-        <GameCard />
-      </Card.Group>
-    </div>
-  </div>
-);
+const MainPage = ({ viewAllGamesButton }) => {
+  const [gamesMoreRecent, setGamesMoreRecent] = useState([]);
+  const [gamesTopRated, setGamesTopRated] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getCard = async () => {
+    setLoading(true);
+    const response = await axios.get('https://gametesting1.herokuapp.com')
+    setGamesMoreRecent(response.data.gamesMoreRecent);
+    setGamesTopRated(response.data.gamesTopRated);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    (async () => {
+      await getCard();
+    })()
+  }, [])
+
+return (
+  loading ? <h1>IT'S LOADING</h1> : (<div className="main mt-5">
+      <div className="main-card-category">
+        <Header as='h2'>Most Popular</Header>
+        <Button  className="view-all-button" onClick={viewAllGamesButton}>View All<span className="button-span">+</span></Button>
+      </div>
+      <div>
+        <Card.Group className="" itemsPerRow={1}>
+          { gamesMoreRecent.map((game) => {
+            return (
+                <GameCard 
+                  key={game.id}
+                  game={game}
+                />
+            )
+          }) }
+        
+        </Card.Group>
+      </div>
+      <div className="main-card-category">
+        <Header as='h2'>Top Rated</Header>
+        <Button className="view-all-button" onClick={viewAllGamesButton}>View All <span className="button-span">+</span></Button>
+      </div>
+      <div>
+        <Card.Group itemsPerRow={1}>
+          { gamesTopRated.map((game) => {
+              return (
+                  <GameCard 
+                    key={game.id}
+                    game={game}
+                  />
+              )
+          }) }
+        </Card.Group>
+      </div>
+      <div className="main-card-category">
+        <Header as='h2'>New Releases</Header>
+        <Button className="view-all-button" onClick={viewAllGamesButton}>View All <span className="button-span">+</span></Button>
+      </div>
+      <div>
+        <Card.Group itemsPerRow={1}>
+
+        </Card.Group>
+      </div>
+    </div>)
+)
+};
 
 export default MainPage;
